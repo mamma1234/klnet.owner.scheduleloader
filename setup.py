a32e55777a7786f074070c47d7117283ca96935d
박대규 <mamma1234@gmail.com>
2019-12-05 10:49:34 +0900

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
+VERSION = '1.0'
+DESCRIPTION = 'Service Sechedule Excel Loader'
+AUTHOR = 'Paul Daekyu, pdkship, mamma',
+AUTHOR_EMAIL = 'mamma1234@gmail.com',
+URL = 'https://github.com/mamma1234',
 SRC_DIR = 'src'
 setup(name='scheduleloader', 
-    version='1.0', 
+    version=VERSION, 
     package_dir={'': SRC_DIR},
+    description = DESCRIPTION,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,    
     packages=find_packages(SRC_DIR))
